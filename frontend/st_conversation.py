5945ecd58ec2f9827c623cf674a3f1c3abfb6390
Daniel Petrov <daniel.petrov18@protonmail.com>
2025-03-15 13:02:06 +0100

    Reworked conversations.

--- frontend/st_conversation.py
+++ frontend/st_conversation.py
@@ -11,8 +11,7 @@ from st_app import load_client # pylint: disable=E0401
 backend_dir = Path(__file__).parent.parent / 'backend'
 sys.path.append(str(backend_dir))
 
-# pylint: disable=E0401
-from conversations import Conversations  
+from conversations import Conversations  # pylint: disable=C0401
 
 @st.cache_resource
 def get_conversations_handler():
@@ -85,18 +84,53 @@ def list_conversations(ids: list[str] = None, offset: int = 0, limit: int = 100)
     except Error as e:
         st.error(f"Unexpected error: {str(e)}")
 
-def delete_conversation(conversation_id: str):
+def fetch_messages(conversation_id: str, display_metadata: bool):
+    """Fetch all messages related to a particular conversation."""
+    try:
+        messages = load_client().conversations.retrieve(conversation_id).results
+
+        if not messages:
+            st.warning(f"No messages found for conversation: {conversation_id}")
+        else:
+            st.markdown(f"**Showing messages for conversation: {conversation_id}**")
+
+            for i, obj in enumerate(messages):
+                with st.expander(
+                    label=f"Message {i+1}: [{obj.id}]",
+                    expanded=True,
+                    icon="📝"
+                ):
+                    if display_metadata:
+                        st.markdown(
+                            # pylint: disable=C0301
+                            f"**Role:** {obj.message.role.upper()}<br>**Content:** {obj.message.content}<br>",
+                            unsafe_allow_html=True
+                        )
+                        st.json(obj.metadata)
+                    else:
+                        st.markdown(
+                            # pylint: disable=C0301
+                            f"**Role:** {obj.message.role.upper()}<br>**Content:** {obj.message.content}",
+                            unsafe_allow_html=True
+                        )
+
+    except R2RException as r2re:
+        st.error(f"Error fetching messages: {str(r2re)}")
+    except Error as e:
+        st.error(f"Unexpected error: {str(e)}")
+
+def delete_conversation(del_conversation_id: str):
     """Delete a conversation."""
     try:
-        load_client().conversations.delete(conversation_id)
+        load_client().conversations.delete(del_conversation_id)
         st.session_state["page_number"] = 0
-        st.success(body=f"Deleted conversation: {conversation_id}", icon="🗑️")
+        st.success(body=f"Deleted conversation: {del_conversation_id}", icon="🗑️")
     except R2RException as r2re:
         st.error(f"Error deleting conversation: {str(r2re)}")
     except Error as e:
         st.error(f"Unexpected error: {str(e)}")
 
-def update_conversation(conversation_id: str, input_name_key: str):
+def update_conversation(update_conversation_id: str, input_name_key: str):
     """
     Change the name of an existing conversation.
     
@@ -107,16 +141,47 @@ def update_conversation(conversation_id: str, input_name_key: str):
     try:
         new_name = st.session_state[input_name_key]
         update_resp = load_client().conversations.update(
-            id=conversation_id,
+            id=update_conversation_id,
             name=new_name
         ).results
         st.session_state["page_number"] = 0
         st.success(
-            body=f"Updated name of {conversation_id} to {update_resp.name}!",
+            body=f"Updated name of {update_conversation_id} to {update_resp.name}!",
             icon="✅"
         )
     except R2RException as r2re:
         st.error(f"Error updating conversation: {str(r2re)}")
+    except Error as e:
+        st.error(f"Unexpected error: {str(e)}")
+
+def export_conversations(conversations_ids: str, output_path: str):
+    """Export conversations to /backend/exports. (Only metadata)"""
+    try:
+        filters = {}
+        if conversations_ids:
+            filters['id'] = [filter_id.strip() for filter_id in conversations_ids.split('\n')]
+
+        get_conversations_handler().export_conversations_to_csv(
+            bearer_token = st.session_state["token"],
+            out = output_path,
+            filters = filters
+        )
+        st.success("Conversations exported successfully")
+    except R2RException as r2re:
+        st.error(f"Error exporting conversations: {str(r2re)}")
+    except Error as e:
+        st.error(f"Unexpected error: {str(e)}")
+
+def export_messages(outpath: str):
+    """Exports all messages of all conversations."""
+    try:
+        get_conversations_handler().export_messages_to_csv(
+            bearer_token = st.session_state["token"],
+            out = outpath
+        )
+        st.success("Messages exported successfully")
+    except R2RException as r2re:
+        st.error(f"Error exporting messages: {str(r2re)}")
     except Error as e:
         st.error(f"Unexpected error: {str(e)}")
 
@@ -129,9 +194,10 @@ if __name__ == "__page__":
     if "page_number" not in st.session_state:
         st.session_state["page_number"] = 0
 
-    t_list, t_exp_conversations, t_exp_msgs  = st.tabs(
+    t_list, t_conv_msgs, t_exp_conversations, t_exp_msgs  = st.tabs(
         [
             "List Conversations",
+            "Conversation messages",
             "Export Conversations",
             "Export Messages"
         ]
@@ -184,60 +250,59 @@ if __name__ == "__page__":
         if conversations_btn:
             list_conversations(ids = filter_ids, offset = curr_offset, limit = curr_limit)
 
-    # with t_exp_conversations:
-    #     st.markdown("**Export Conversations Metadata to CSV**")
+    with t_conv_msgs:
+        st.markdown("**Conversation messages**")
 
-    #     filename = st.text_input(
-    #         label = "export filename",
-    #         placeholder = "Ex. conversations",
-    #         max_chars = 20,
-    #         help = "Name of the exported file without extension"
-    #     )
+        conversation_id = st.text_input(
+            label = "Conversation ID",
+            placeholder = "Ex. conversation_id"
+        )
 
-    #     filter_ids = st.text_input(
-    #         label = "conversation ids",
-    #         placeholder = "Ex. conversation_id1,conversation_id2,conversation_id3 (optional)",
-    #         help = "Comma separated list of conversation ids"
-    #     )
+        include_metadata = st.checkbox(
+            label="Include metadata",
+            value=False,
+        )
 
-    #     export_conversations_btn = st.button("Export conversations")
-    #     if export_conversations_btn:
-    #         try:
-    #             filters = {}
+        if st.button("Get messages", type="primary"):
+            if not conversation_id:
+                st.warning("Please enter a conversation ID")
+            else:
+                fetch_messages(conversation_id, include_metadata)
 
-    #             if filter_ids:
-    #                 filters['id'] = [filter_id.strip() for filter_id in filter_ids.split(',')]
+    with t_exp_conversations:
+        st.markdown("**Export Conversations Metadata to CSV**")
 
-    #             get_conversations_handler().export_conversations_to_csv(
-    #                 bearer_token = st.session_state["token"],
-    #                 out = filename,
-    #                 filters = filters
-    #             )
-    #             st.success("Conversations exported successfully")
-    #         except R2RException as r2re:
-    #             st.error(f"Error exporting conversations: {str(r2re)}")
-    #         except Error as e:
-    #             st.error(f"Unexpected error: {str(e)}")
+        filename = st.text_input(
+            label = "export filename",
+            placeholder = "Ex. conversations",
+            max_chars = 20,
+            help = "Name of the exported file without extension"
+        )
 
-    # with t_exp_msgs:
-    #     st.markdown("**Export Conversation Messages to CSV**")
+        filter_ids = st.text_area(
+            label = "conversation ids",
+            placeholder = "conversation_id1\nconversation_id2\n... (optional)",
+            help = "Conversation ids on each line"
+        )
 
-    #     filename = st.text_input(
-    #         label = "export filename",
-    #         placeholder = "Ex. messages",
-    #         max_chars = 20,
-    #         help = "Name of the exported file without extension"
-    #     )
+        if st.button("Export conversations"):
+            if not filename:
+                st.warning("Please enter a filename")
+            else:
+                export_conversations(filter_ids, filename)
 
-    #     export_msgs_btn = st.button("Export messages")
-    #     if export_msgs_btn:
-    #         try:
-    #             get_conversations_handler().export_messages_to_csv(
-    #                 bearer_token = st.session_state["token"],
-    #                 out = filename
-    #             )
-    #             st.success("Messages exported successfully")
-    #         except R2RException as r2re:
-    #             st.error(f"Error exporting messages: {str(r2re)}")
-    #         except Error as e:
-    #             st.error(f"Unexpected error: {str(e)}")
+    with t_exp_msgs:
+        st.markdown("**Export Conversation Messages to CSV**")
+
+        filename = st.text_input(
+            label = "export filename",
+            placeholder = "Ex. messages",
+            max_chars = 20,
+            help = "Name of the exported file without extension"
+        )
+
+        if st.button("Export messages"):
+            if not filename:
+                st.warning("Please enter a filename")
+            else:
+                export_messages(filename)
